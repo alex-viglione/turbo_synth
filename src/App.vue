540ac81280ab405d866dc16a1f54ad6643163139
Alex Viglione <alexxe33@gmail.com>
2020-06-09 22:00:30 +0200

    Github init

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,78 @@
 <template>
 	<div id="app">
-		<h1>Testing</h1>
-		<button @click="displayNotes">Log notes</button>
+		<h1>Basic oscillator test</h1>
+		<label for="waveType">Choose a wave type:</label>
+
+		<select
+			name="waveType"
+			id="waveType"
+			v-model="wave"
+		>
+			<option value="sine">sine</option>
+			<option value="square">square</option>
+			<option value="triangle">triangle</option>
+			<option value="sawtooth">sawtooth</option>
+		</select>
+
+		<hr>
+
+		<button
+			v-for="note in noteValues"
+			@click="playSound(note)"
+		>Play {{getKeyByValue(noteValues, note)}}</button>
+
 	</div>
 </template>
 
 <script>
-import noteValues from './assets/notevalues.js';
+import { noteValues } from './assets/notevalues.js';
 
 export default {
 	name: 'App',
 	data() {
 		return {
-			noteValues
+			noteValues,
+			selectedNote: null,
+			wave: 'sine'
 		}
 	},
-	components: {
-	},
 	methods: {
-		displayNotes() {
-			console.log(this.noteValues)
+		getKeyByValue(object, value) {
+			return Object.keys(object).find(key => object[key] === value);
+		}
+		,
+		playSound(note) {
+			const context = new AudioContext();
+			const o = context.createOscillator();
+			const g = context.createGain();
+
+			o.connect(g);
+			o.type = this.wave;
+			const frequency = note;
+
+			o.frequency.value = frequency;
+			g.connect(context.destination);
+			o.start(0);
+			setTimeout(() => {
+				g.gain.exponentialRampToValueAtTime(
+					0.00001, context.currentTime + 1
+				);
+			}, 50);
 		}
 	}
 }
 </script>
 
 <style>
+html {
+	background-color: #2c3e50;
+}
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
+	color: white;
 	margin-top: 60px;
 }
 </style>
